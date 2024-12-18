82569535cfbc4a7ee326448da81331c632d4ab8f
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2024-12-18 03:44:33 -0300

    #1 Adding seeder for restaurants and dishes

--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -1,3 +1,4 @@
+using Restaurants.Infrastructure.Seeders;
 using Restaurants.Infrastructure.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,11 @@ builder.Services.AddControllers();
 builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
+
+var scope = app.Services.
+CreateScope();
+var seeder = scope.ServiceProvider.GetRequiredService<IRestaurantSeeder>();
+await seeder.Seed();
 
 // Configure the HTTP request pipeline.
 
--- Restaurants.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ Restaurants.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Restaurants.Infrastructure.Seeders;
 using Restaurants.Infrastructure.Persistence;
 
 namespace Restaurants.Infrastructure.Extensions;
@@ -11,5 +12,7 @@ public static class ServiceCollectionExtensions
 	{
 		var connectionString = configuration.GetConnectionString("RestaurantsDB");
 		services.AddDbContext<RestaurantsDbContext>(options => options.UseNpgsql(connectionString, pgOptions => pgOptions.MigrationsAssembly("Restaurants.Infrastructure")));
+
+		services.AddScoped<IRestaurantSeeder, RestaurantSeeder>();
 	}
 }
