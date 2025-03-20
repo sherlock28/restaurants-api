5f9bcacf29d3d124702740b485d8d051cfdf5930
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2025-03-20 07:51:01 -0300

    #1 Adding validations with 'FluentValidation'

--- Restaurants.Application/Extensions/ServiceCollectionExtensions.cs
+++ Restaurants.Application/Extensions/ServiceCollectionExtensions.cs
@@ -1,3 +1,5 @@
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using Microsoft.Extensions.DependencyInjection;
 using Restaurants.Application.Restaurants;
 
@@ -7,7 +9,11 @@ public static class ServiceCollectionExtensions
 {
 	public static void AddApplication(this IServiceCollection services)
 	{
+		var applicationAssembly = typeof(ServiceCollectionExtensions).Assembly;
+
 		services.AddScoped<IRestaurantsService, RestaurantsService>();
-		services.AddAutoMapper(typeof(ServiceCollectionExtensions).Assembly);
+		services.AddAutoMapper(applicationAssembly);
+		services.AddValidatorsFromAssembly(applicationAssembly)
+			.AddFluentValidationAutoValidation();
 	}
 }
---
+++ Restaurants.Application/Restaurants/Validators/CreateRestaurantDtoValidator.cs
@@ -0,0 +1,39 @@
+using FluentValidation;
+using Restaurants.Application.Restaurants.Dtos;
+
+namespace Restaurants.Application.Restaurants.Validators;
+
+public class CreateRestaurantDtoValidator : AbstractValidator<CreateRestaurantDto>
+{
+	private readonly string[] validCategories = { "Fast Food", "Italian", "Chinese", "Mexican", "American", "Japanese", "Indian", "Thai", "Greek", "French", "Vegetarian", "Vegan", "Other" };
+
+	public CreateRestaurantDtoValidator()
+	{
+		RuleFor(dto => dto.Name)
+			.Length(3, 100)
+			.WithMessage("Name field must be between 3 and 100 characters.");
+
+		RuleFor(dto => dto.Description)
+			.NotEmpty().WithMessage("Description is required.");
+
+		RuleFor(dto => dto.Category)
+			.Custom((category, context) =>
+			{
+				if (!validCategories.Contains(category))
+				{
+					context.AddFailure("Category", "Invalid category: Please choose from the valid categories.");
+				}
+			});
+
+		RuleFor(dto => dto.ContactEmail)
+			.EmailAddress().WithMessage("Please provide a valid email address.");
+
+		RuleFor(dto => dto.ContactNumber)
+			.Matches(@"^\+?[0-9]{3}-?[0-9]{6,12}$")
+			.WithMessage("Invalid phone number.");
+
+		RuleFor(dto => dto.PostalCode)
+			.Matches(@"^\d{2}-\d{3}$")
+			.WithMessage("Please provide a valid postal code (XX-XXX).");
+	}
+}
