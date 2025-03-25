04d943a2d639e7e2da9d542b32a43578086b8df6
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2025-03-25 06:19:36 -0300

    #1 Adding Swagger

--- Restaurants.API/Controllers/RestaurantsController.cs
+++ Restaurants.API/Controllers/RestaurantsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Restaurants.Application.Restaurants.Commands.CreateRestaurant;
 using Restaurants.Application.Restaurants.Commands.DeleteRestaurant;
 using Restaurants.Application.Restaurants.Commands.UpdateRestaurant;
+using Restaurants.Application.Restaurants.Dtos;
 using Restaurants.Application.Restaurants.Queries.GetAllRestaurants;
 using Restaurants.Application.Restaurants.Queries.GetRestaurantById;
 
@@ -13,14 +14,14 @@ namespace Restaurants.API.Controllers;
 public class RestaurantsController(IMediator mediator) : ControllerBase
 {
 	[HttpGet]
-	public async Task<IActionResult> GetAll()
+	public async Task<ActionResult<IEnumerable<RestaurantDto>>> GetAll()
 	{
 		var restaurants = await mediator.Send(new GetAllRestaurantsQuery());
 		return Ok(restaurants);
 	}
 
 	[HttpGet("{id}")]
-	public async Task<IActionResult> GetById([FromRoute] int id)
+	public async Task<ActionResult<RestaurantDto?>> GetById([FromRoute] int id)
 	{
 		var restaurant = await mediator.Send(new GetRestaurantByIdQuery(id));
 		if (restaurant is null)
@@ -30,6 +31,7 @@ public class RestaurantsController(IMediator mediator) : ControllerBase
 	}
 
 	[HttpPost]
+	[ProducesResponseType(StatusCodes.Status201Created)]
 	public async Task<IActionResult> CreateRestaurant([FromBody] CreateRestaurantCommand command)
 	{
 		int id = await mediator.Send(command);
@@ -38,6 +40,8 @@ public class RestaurantsController(IMediator mediator) : ControllerBase
 	}
 
 	[HttpDelete("{id}")]
+	[ProducesResponseType(StatusCodes.Status204NoContent)]
+	[ProducesResponseType(StatusCodes.Status404NotFound)]
 	public async Task<IActionResult> DeleteRestaurant([FromRoute] int id)
 	{
 		var isDeleted = await mediator.Send(new DeleteRestaurantCommand(id));
@@ -48,6 +52,8 @@ public class RestaurantsController(IMediator mediator) : ControllerBase
 	}
 
 	[HttpPatch("{id}")]
+	[ProducesResponseType(StatusCodes.Status204NoContent)]
+	[ProducesResponseType(StatusCodes.Status404NotFound)]
 	public async Task<IActionResult> UpdateRestaurant([FromRoute] int id, [FromBody] UpdateRestaurantCommand command)
 	{
 		command.Id = id;
--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -1,5 +1,4 @@
 using Serilog;
-using Serilog.Events;
 using Restaurants.Infrastructure.Seeders;
 using Restaurants.Application.Extensions;
 using Restaurants.Infrastructure.Extensions;
@@ -9,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
@@ -18,14 +18,19 @@ builder.Host.UseSerilog((context, configuration)
 
 var app = builder.Build();
 
-app.UseSerilogRequestLogging();
-
 var scope = app.Services.
 CreateScope();
 var seeder = scope.ServiceProvider.GetRequiredService<IRestaurantSeeder>();
 await seeder.Seed();
 
 // Configure the HTTP request pipeline.
+app.UseSerilogRequestLogging();
+
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
