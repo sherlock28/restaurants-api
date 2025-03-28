c0688a35fdbb3d4bdc6940cda0155917ae33308a
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2025-03-28 08:28:32 -0300

    #1 Adding endpoint to get all the dishes of a restaurant and a specific dish

--- Restaurants.Application/Dishes/Dtos/DishDto.cs
+++ Restaurants.Application/Dishes/Dtos/DishDto.cs
@@ -8,6 +8,7 @@ public class DishDto
 	public string Name { get; set; } = default!;
 	public string Description { get; set; } = default!;
 	public decimal Price { get; set; }
+
 	public int? KiloCalories { get; set; }
 
 	public static DishDto FromEntity(Dish dish)
---
+++ Restaurants.Application/Dishes/Queries/GetDishByIdForRestaurant/GetDishByIdForRestaurantQueryHandler.cs
@@ -0,0 +1,37 @@
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Restaurants.Domain.Exceptions;
+using Restaurants.Domain.Repositories;
+using Restaurants.Application.Dishes.Dtos;
+using Restaurants.Domain.Entities;
+using AutoMapper;
+
+namespace Restaurants.Application.Dishes.Queries.GetDishByIdForRestaurant;
+
+public class GetDishByIdForRestaurantQueryHandler(IRestaurantsRepository restaurantsRepository,
+	IMapper mapper,
+	ILogger<GetDishByIdForRestaurantQueryHandler> logger) : IRequestHandler<GetDishByIdForRestaurantQuery, DishDto>
+{
+	public async Task<DishDto> Handle(GetDishByIdForRestaurantQuery request, CancellationToken cancellationToken)
+	{
+		logger.LogInformation("Getting dish {dishId} for restaurant {restaurantId}", request.DishId, request.RestaurantId);
+
+		var restaurant = await restaurantsRepository.GetByIdAsync(request.RestaurantId);
+
+		if (restaurant == null)
+		{
+			throw new NotFoundException(nameof(Restaurant), request.RestaurantId.ToString());
+		}
+
+		var dish = restaurant.Dishes.FirstOrDefault(d => d.Id == request.DishId);
+
+		if (dish == null)
+		{
+			throw new NotFoundException(nameof(Dish), request.DishId.ToString());
+		}
+
+		var result = mapper.Map<DishDto>(dish);
+
+		return result;
+	}
+}
