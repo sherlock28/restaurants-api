4a8229cb228e856a0bc7df8510d34235e33937ee
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2025-04-03 00:21:13 -0300

    #1 Adding endpoint to remove a role from a user

--- Restaurants.API/Controllers/IdentityController.cs
+++ Restaurants.API/Controllers/IdentityController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 using Restaurants.Domain.Constants;
 using Restaurants.Application.Users.Commands.AssignUserRole;
+using Restaurants.Application.Users.Commands.UnassignUserRole;
 using Restaurants.Application.Users.Commands.UpdateUserDetails;
 
 namespace Restaurants.API.Controllers;
@@ -27,6 +28,17 @@ public class IdentityController(IMediator mediator) : ControllerBase
 	[ProducesResponseType(StatusCodes.Status204NoContent)]
 	[ProducesResponseType(StatusCodes.Status404NotFound)]
 	public async Task<IActionResult> AssignUserRole(AssignUserRoleCommand command)
+	{
+		await mediator.Send(command);
+
+		return NoContent();
+	}
+
+	[HttpDelete("userRole")]
+	[Authorize(Roles = UserRoles.Admin)]
+	[ProducesResponseType(StatusCodes.Status204NoContent)]
+	[ProducesResponseType(StatusCodes.Status404NotFound)]
+	public async Task<IActionResult> UnassignUserRole(UnassignUserRoleCommand command)
 	{
 		await mediator.Send(command);
 
