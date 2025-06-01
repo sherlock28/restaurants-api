008da9a10a4c245156cca1256191715df15fb2f3
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2025-06-01 05:33:42 -0300

    #1 Fix: assign correct NormalizedName to each role

--- Restaurants.Infrastructure/Seeders/RestaurantSeeder.cs
+++ Restaurants.Infrastructure/Seeders/RestaurantSeeder.cs
@@ -38,11 +38,11 @@ internal class RestaurantSeeder(RestaurantsDbContext dbContext) : IRestaurantSee
 				},
 				new (UserRoles.Owner)
 				{
-					NormalizedName = UserRoles.User.ToUpper(),
+					NormalizedName = UserRoles.Owner.ToUpper(),
 				},
 				new (UserRoles.Admin)
 				{
-					NormalizedName = UserRoles.User.ToUpper(),
+					NormalizedName = UserRoles.Admin.ToUpper(),
 				},
 			];
 
