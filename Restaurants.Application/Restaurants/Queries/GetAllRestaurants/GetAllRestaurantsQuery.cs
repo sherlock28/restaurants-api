3db2c22326419094472fbc5e31d6a6fd23dc970b
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2025-07-11 02:53:30 -0300

    #1 Adding validation for pagination parameters

--- Restaurants.Application/Restaurants/Queries/GetAllRestaurants/GetAllRestaurantsQuery.cs
+++ Restaurants.Application/Restaurants/Queries/GetAllRestaurants/GetAllRestaurantsQuery.cs
@@ -7,6 +7,6 @@ namespace Restaurants.Application.Restaurants.Queries.GetAllRestaurants;
 public class GetAllRestaurantsQuery : IRequest<PagedResult<RestaurantDto>>
 {
 	public string? SearchPhrase{ get; set; }
-	public int PageNumber { get; set; } = 1;
-	public int PageSize { get; set; } = 10;
+	public int PageNumber { get; set; }
+	public int PageSize { get; set; }
 }
